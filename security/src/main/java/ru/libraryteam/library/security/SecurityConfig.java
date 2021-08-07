3a0afeacbd9078af278c5045cd43c935233bfa47
edward4324 <38614548+edward4324@users.noreply.github.com>
2021-08-07 15:54:02 +0300

    Added CORS support for http://localhost:8080 try 3 fixed

--- security/src/main/java/ru/libraryteam/library/security/SecurityConfig.java
+++ security/src/main/java/ru/libraryteam/library/security/SecurityConfig.java
@@ -96,7 +96,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
   @Bean
   CorsConfigurationSource corsConfigurationSource() {
     CorsConfiguration configuration = new CorsConfiguration();
-    configuration.setAllowedOrigins(Arrays.asList("http://localhost:8000/"));
+    configuration.setAllowedOrigins(Arrays.asList("http://localhost:8080/"));
     configuration.setAllowedMethods(Arrays.asList("GET","POST", "PUT", "DELETE"));
     UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
     source.registerCorsConfiguration("/**", configuration);
