b079b90397a9020983c9a504fbf0f084a3932708
edward4324 <38614548+edward4324@users.noreply.github.com>
2021-08-07 17:35:31 +0300

    Added CORS support for http://localhost:8080 try 4

--- src/main/java/ru/libraryteam/library/LibraryApplication.java
+++ src/main/java/ru/libraryteam/library/LibraryApplication.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 @Configuration(proxyBeanMethods = false)
@@ -13,6 +14,16 @@ public class LibraryApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(LibraryApplication.class, args);
+  }
+
+  @Bean
+  public WebMvcConfigurer corsConfigurer() {
+    return new WebMvcConfigurerAdapter() {
+      @Override
+      public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedOrigins("http://localhost:8080");
+      }
+    };
   }
 
 }
