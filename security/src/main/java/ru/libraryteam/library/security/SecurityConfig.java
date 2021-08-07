6201ac29fbaaf740444e87736071a429d111f7d9
edward4324 <38614548+edward4324@users.noreply.github.com>
2021-08-07 22:57:30 +0300

    Issues fixed

--- security/src/main/java/ru/libraryteam/library/security/SecurityConfig.java
+++ security/src/main/java/ru/libraryteam/library/security/SecurityConfig.java
@@ -70,8 +70,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
   @Override
   protected void configure(HttpSecurity http) throws Exception {
     http
-      .cors().configurationSource(corsConfigurationSource())
-      .disable()
+      .cors()
+      .and()
       .csrf().disable()
       .formLogin()
       .loginProcessingUrl("/auth/login")
